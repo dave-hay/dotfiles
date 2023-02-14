8ec956e11415743341a8a16d46e7a69d65d31dfa
Dave Hay <davehay93@gmail.com>
2023-02-14 14:35:00 -0800

    ls colors

---
+++ system/ls_colors.zsh
@@ -0,0 +1,18 @@
+# megafile -> https://github.com/trapd00r/LS_COLORS#installation
+
+export CLICOLOR=1
+export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
+
+# $ man ls -- shows how LSCOLORS works
+# 1.   directory
+# 2.   symbolic link
+# 3.   socket
+# 4.   pipe
+# 5.   executable
+# 6.   block special
+# 7.   character special
+# 8.   executable with setuid bit set
+# 9.   executable with setgid bit set
+# 10.  directory writable to others, with sticky bit
+# 11.  directory writable to others, without sticky bit
+
