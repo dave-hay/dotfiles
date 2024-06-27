c5ec583c1f50d67181094867b0013a159f1c2b89
dave-hay <davehay93@gmail.com>
2024-06-26 19:13:12 -0700

    git alias's

---
+++ system/git.utils.zsh
@@ -0,0 +1,25 @@
+alias g='git'
+
+alias gac='git add . && git commit -m'
+alias glb='git checkout -'
+alias gnb='git checkout -b'
+alias gcl='git rm -rf --cached .'
+alias gp='git push -u origin main'
+
+alias ga='git add'
+alias gaa='git add --all'
+alias gav='git add --verbose'
+
+alias gcam='git commit --all --message'
+alias gcas='git commit --all --signoff'
+alias gcasm='git commit --all --signoff --message'
+alias gcmsg='git commit --message'
+alias gcsm='git commit --signoff --message'
+alias gc='git commit --verbose'
+
+alias gb='git branch'
+alias gba='git branch --all'
+alias gbd='git branch --delete'
+alias gbD='git branch --delete --force'
+
+alias gi='git init'
